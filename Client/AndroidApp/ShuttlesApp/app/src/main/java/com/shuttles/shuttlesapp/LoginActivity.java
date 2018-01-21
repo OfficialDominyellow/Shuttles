0b03c8e28fc16ef1821af1295810b46afe8508e3
OfficialDominyellow <dominyellow@gmail.com>
2018-01-21 15:40:33 +0900

    Make navigation menu and Main Activity
    Make navigation menu.
    Make Main(dashboard) Activity
    
    will design dashboard layout soon.

--- Client/AndroidApp/ShuttlesApp/app/src/main/java/com/shuttles/shuttlesapp/LoginActivity.java
+++ Client/AndroidApp/ShuttlesApp/app/src/main/java/com/shuttles/shuttlesapp/LoginActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by domin on 2018-01-12.
@@ -19,5 +21,14 @@ public class LoginActivity extends Activity {
         //call splash
         Intent splashIntent = new Intent(this, SplashActivity.class);
         startActivity(splashIntent);
+
+        Button btnLogin = (Button)findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent dashboardIntent = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(dashboardIntent);
+            }
+        });
     }
 }
